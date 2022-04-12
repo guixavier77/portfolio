a6b5a41d7d4a74d102e3bf44b2046524df1d014a
guixaiver77 <xguilherme1@gmail.com>
2022-04-12 19:25:27 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,30 @@ function initMenuMobile(){
 
 
 initMenuMobile();
+
+
+function getDistanceFromTheTop(element){
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(DistanceFromTheTop){
+  window.scroll({
+    top: DistanceFromTheTop,
+    behavior: "smooth",
+  });
+}
+function scrollToSection(event){
+  event.preventDefault();
+  const DistanceFromTheTop = getDistanceFromTheTop(event.target);
+  nativeScroll(DistanceFromTheTop);
+ 
+}
+
+const menuLinks = document.querySelectorAll('.menu-lista a[href^="#"]');
+
+menuLinks.forEach((link)=>{
+  link.addEventListener("click", scrollToSection);
+});
+
+
