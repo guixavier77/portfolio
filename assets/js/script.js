7b0dee3c7abf183f179af2685a8778d0c0d5c442
guixaiver77 <xguilherme1@gmail.com>
2022-04-15 14:21:56 -0300

    correçao barra lateral

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import initScrollToSection from "./modules/scroll-to-section.js"
 import initAnimeScroll from "./modules/anime-scroll.js";
-import initMouseOver from "./modules/mouse-over.js";
 
 
 initMenuMobile();
 initScrollToSection();
 initAnimeScroll();
-initMouseOver();
